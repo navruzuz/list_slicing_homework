10f9c146d41e591c4a0902384dc1d13f9149297e
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-28 16:10:47 +0500

    bajarildi

--- Slicing_list10.py
+++ Slicing_list10.py
@@ -7,5 +7,5 @@ def main(list1,n):
     Returns:
         list: return answer.
     """
-    return list1[-1:n-1:-1]
-print(main([1,2,3,4,5,6,7,8,9],2))
+    return list1[:n-1:-1]
+print(main([1,2,3,4,5,6,7,8,9],6))
